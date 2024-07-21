8df43709903414bd392ce8b5b491ceef5f1f2111
zhubeilife <zhubeilife@gmail.com>
2024-07-21 10:26:01 +0800

    chanfge objdump not alias

--- scripts/platform/nemu.mk
+++ scripts/platform/nemu.mk
@@ -22,7 +22,7 @@ insert-arg: image
 	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) "$(MAINARGS_PLACEHOLDER)" "$(mainargs)"
 
 image: image-dep
-	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+	@$(OBJDUMP) -M no-aliases -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 

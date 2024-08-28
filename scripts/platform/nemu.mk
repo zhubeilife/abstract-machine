2dcb7299a49e2e4bdc067913c4137b719adbb0c7
zhubeilife <zhubeilife@gmail.com>
2024-08-28 19:37:20 +0800

    add read elf file option

--- scripts/platform/nemu.mk
+++ scripts/platform/nemu.mk
@@ -13,6 +13,7 @@ LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
+NEMUFLAGS += -e $(IMAGE).elf
 # --batch              run with batch mode
 # NEMUFLAGS += --batch
 

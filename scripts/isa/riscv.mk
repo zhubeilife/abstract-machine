ae7140e5f51b661defaad9c89eaa0aaa9f5d0e95
zhubeilife <zhubeilife@gmail.com>
2024-09-03 21:54:31 +0800

    fix wrong set

--- scripts/isa/riscv.mk
+++ scripts/isa/riscv.mk
@@ -1,12 +1,12 @@
 OS_CHECK := $(shell lsb_release -d 2>/dev/null | grep -q "Ubuntu 18.04" && echo "Ubuntu 18.04" || echo "Not Ubuntu 18.04")
 # hack way to fit using at ubuntu 18.04
-ifeq ($(GCC_VERSION), "Ubuntu 18.04")
+$(info OS ${OS_CHECK})
+ifeq ($(OS_CHECK), Ubuntu 18.04)
     CROSS_COMPILE := riscv64-unknown-linux-gnu-
 else
 	CROSS_COMPILE := riscv64-linux-gnu-
 endif
 
-CROSS_COMPILE := riscv64-unknown-linux-gnu-
 COMMON_CFLAGS := -fno-pic -march=rv64g -mcmodel=medany -mstrict-align
 CFLAGS        += $(COMMON_CFLAGS) -static
 ASFLAGS       += $(COMMON_CFLAGS) -O0

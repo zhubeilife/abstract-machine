89da4d4e56373afe4cbd4252cc40773eb863887b
BryanZhang <bryan.zhang@hairobotics.com>
2024-09-03 11:46:26 +0800

    hack to compile on ubuntu 18.04

--- scripts/isa/riscv.mk
+++ scripts/isa/riscv.mk
@@ -1,4 +1,12 @@
-CROSS_COMPILE := riscv64-linux-gnu-
+OS_CHECK := $(shell lsb_release -d 2>/dev/null | grep -q "Ubuntu 18.04" && echo "Ubuntu 18.04" || echo "Not Ubuntu 18.04")
+# hack way to fit using at ubuntu 18.04
+ifeq ($(GCC_VERSION), "Ubuntu 18.04")
+    CROSS_COMPILE := riscv64-unknown-linux-gnu-
+else
+	CROSS_COMPILE := riscv64-linux-gnu-
+endif
+
+CROSS_COMPILE := riscv64-unknown-linux-gnu-
 COMMON_CFLAGS := -fno-pic -march=rv64g -mcmodel=medany -mstrict-align
 CFLAGS        += $(COMMON_CFLAGS) -static
 ASFLAGS       += $(COMMON_CFLAGS) -O0

86587790bde0b4792d1a986568673cb30c3e93f8
BryanZhang <bryan.zhang@hairobotics.com>
2024-09-18 18:30:04 +0800

    improve timer

--- am/src/platform/nemu/ioe/timer.c
+++ am/src/platform/nemu/ioe/timer.c
@@ -5,8 +5,12 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint64_t us = inl(RTC_ADDR) + ((uint64_t)inl(RTC_ADDR + 4) << 32);
-  uptime->us = us;
+  // first read high and low 32-bit of the RTC
+  // because the rtc_io_handler has if (!is_write && offset == 4) logic
+  // detial can be found https://www.cnblogs.com/nosae/p/17066439.html#%E7%9C%8B%E7%9C%8Bnemu%E8%B7%91%E5%A4%9A%E5%BF%AB
+  uint32_t high = inl(RTC_ADDR+4);
+  uint32_t low = inl(RTC_ADDR);
+  uptime->us = (uint64_t)low + (((uint64_t)high) << 32);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {

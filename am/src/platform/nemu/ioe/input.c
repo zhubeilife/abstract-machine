ab9c2d292b0541fdf009c749bc965508261af6c3
zhubeilife <zhubeilife@gmail.com>
2024-09-19 22:02:15 +0800

    complte keyboard

--- am/src/platform/nemu/ioe/input.c
+++ am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,8 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  // uint32_t am_scancode = keymap[scancode] | (is_keydown ? KEYDOWN_MASK : 0);
+  uint32_t am_scancode = inl(KBD_ADDR);
+  kbd->keydown = (am_scancode & KEYDOWN_MASK) ? true : false;
+  kbd->keycode = am_scancode & (~KEYDOWN_MASK);
 }

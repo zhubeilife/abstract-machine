ec5fedf877fbaad0aad2203f25db2b9bc512d9c4
zhubeilife <zhubeilife@gmail.com>
2025-07-01 09:43:35 +0800

    pa3.2 syscal

--- am/include/arch/riscv.h
+++ am/include/arch/riscv.h
@@ -16,15 +16,16 @@ struct Context {
   void *pdir;
 };
 
+// # define ARGS_ARRAY ("ecall", "a7", "a0", "a1", "a2", "a0")
 #ifdef __riscv_e
 #define GPR1 gpr[15] // a5
 #else
 #define GPR1 gpr[17] // a7
 #endif
 
-#define GPR2 gpr[0]
-#define GPR3 gpr[0]
-#define GPR4 gpr[0]
-#define GPRx gpr[0]
+#define GPR2 gpr[10]   // a0
+#define GPR3 gpr[11]   // a1
+#define GPR4 gpr[12]   // a2
+#define GPRx gpr[10]  // a0
 
 #endif
--- am/src/riscv/nemu/cte.c
+++ am/src/riscv/nemu/cte.c
@@ -3,6 +3,8 @@
 #include <klib.h>
 #include <math.h>
 
+#include "klib-macros.h"
+
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
@@ -10,7 +12,20 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
       // 对于mips32的syscall和riscv32的ecall, 保存的是自陷指令的PC, 因此软件需要在适当的地方对保存的PC加上4, 使得将来返回到自陷指令的下一条指令.
-      case 11: ev.event = EVENT_YIELD; c->mepc += 4; break;
+      // ENVIRONMENT_CALL_FROM_M_MODE 11
+      case 11: {
+        int type = c->GPR1;
+        if (type < 0) {
+          ev.event = EVENT_YIELD;
+        } else if (type >= 0 && type <= 16) {
+          ev.event = EVENT_SYSCALL;
+        }else {
+          printf("not support: c->GPR1 = %d\n", c->GPR1);
+          panic("not support: c->GPR1 = %d\n");
+        }
+        c->mepc += 4;
+        break;
+      }
       default: ev.event = EVENT_ERROR; break;
     }
 
@@ -41,10 +56,7 @@ void yield() {
 #ifdef __riscv_e
   asm volatile("li a5, -1; ecall");
 #else
-  // 在RISC-V的用户程序中，使用ecall指令请求syscall。操作系统提供不同功能的syscall，对应不同的请求调用号。因此请求前，需要向a7寄存器中写入所请求的syscall的调用号
-  // 这里强制改成了11，为了让difftest通过
-  // rv的手册发现是自己没好好看手册的问题，mcause=0xb表示的是environment call from M-mode，由于我们全程都在M模式下跑，因此ecall对应的mcause就是0xb
-  asm volatile("li a7, 11; ecall");
+  asm volatile("li a7, -1; ecall");
 #endif
 }
 
